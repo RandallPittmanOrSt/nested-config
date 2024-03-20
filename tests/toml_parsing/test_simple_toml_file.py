5f8cd99b4330af78ff259be79f891475cac07b78
Randall Pittman <pittmara@oregonstate.edu>
2024-03-20 15:54:19 -0700

    variable name changes

--- tests/toml_parsing/test_simple_toml_file.py
+++ tests/toml_parsing/test_simple_toml_file.py
@@ -5,27 +5,27 @@ from pathlib import Path
 import pydantic
 import pydantic_plus
 
-BASIC_POINT_FILE = Path(__file__).parent / "point.toml"
+TOML_PATH = Path(__file__).parent / "simple_house.toml"
 
 
-class Point(pydantic_plus.BaseModel):
+class House(pydantic_plus.BaseModel):
     name: str
     x: int
     y: int
 
 
-class Point2(pydantic.BaseModel):
+class House2(pydantic.BaseModel):
     name: str
     x: int
     y: int
 
-POINT_DATA = {"name": "home", "x": 3, "y": 2}
+HOUSE_DATA = {"name": "home", "length": 30, "width": 20}
 
 def test_basic_point_file():
     """Test creating a Point with the from_toml method of pydantic_plus.BaseModel"""
-    assert Point.from_toml(BASIC_POINT_FILE) == Point(**POINT_DATA)
+    assert House.from_toml(TOML_PATH) == House(**HOUSE_DATA)
 
 
 def test_basic_point_file2():
     """Test creating a Point2 with pydantic_plus.obj_from_toml"""
-    assert pydantic_plus.obj_from_toml(BASIC_POINT_FILE, Point2) == Point2(**POINT_DATA)
+    assert pydantic_plus.obj_from_toml(TOML_PATH, House2) == House2(**HOUSE_DATA)
