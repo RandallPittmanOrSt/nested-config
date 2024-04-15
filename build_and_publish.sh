d175e249fc7c9789bc5fbdc1bf336df7780799fe
Randall Pittman <pittmara@oregonstate.edu>
2024-04-15 13:54:43 -0700

    update script

--- build_and_publish.sh
+++ build_and_publish.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 SCRIPTNAME="$(basename "$0")"
 
+log() {
+  local msg="${1:-}"
+  local loglevel="${2:-INFO}"
+  echo "$SCRIPTNAME $loglevel: $msg" >&2
+}
+
 die() {
-  echo "$SCRIPTNAME Error: ${1:-}" >&2
+  log "${1:-}" ERROR
   exit "${2:-1}"
 }
 
@@ -11,9 +17,9 @@ echo "Publishing to the $REPO repo."
 
 REPO_UPPER="$(echo "$REPO" | tr '[:lower:]' '[:upper:]')"
 # shellcheck disable=SC1091
-. .env || die "No .env file. Create one that exports POETRY_PYPI_TOKEN_${REPO_UPPER} "
+. .env 2>/dev/null || die "No .env file. Create one that exports POETRY_PYPI_TOKEN_${REPO_UPPER} "
 
 declare -n REPO_TOKEN_ENVVAR=POETRY_PYPI_TOKEN_${REPO_UPPER}
-[ -z "${!REPO_TOKEN_ENVVAR}" ] && die "$REPO_TOKEN_ENVVAR was apparently not set in the .env file"
+[ -z "$REPO_TOKEN_ENVVAR" ] && die "${!REPO_TOKEN_ENVVAR} was apparently not set in the .env file"
 poetry build || die "Problem with build step"
 poetry publish --repository "$REPO"
