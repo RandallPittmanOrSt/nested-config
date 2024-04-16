805a61b7936b3c2f65deb3f3a713174758e9708d
Randall Pittman <pittmara@oregonstate.edu>
2024-04-16 16:26:12 -0700

    Remove unused pydantic stuff

--- src/nested_config/_pydantic.py
+++ src/nested_config/_pydantic.py
@@ -1,7 +1,7 @@
 """_pyd_compat.py - Functions and types to assist with Pydantic 1/2 compatibility"""
 
 from pathlib import PurePath, PurePosixPath, PureWindowsPath
-from typing import Any, Dict, Optional, Protocol, Type, TypeVar
+from typing import Any, Dict, Optional, Type, TypeVar
 
 import pydantic
 import pydantic.errors
@@ -17,38 +17,11 @@ PathT = TypeVar("PathT", bound=PurePath)
 PYDANTIC_1 = Version(pydantic.VERSION) < Version("2.0")
 
 
-class HasAnnotation(Protocol):
-    """Protocol will allow some Pydantic 2.0 compatibility down the road"""
-
-    annotation: Optional[Type[Any]]
-
-
 if PYDANTIC_1:
     FieldInfo_: TypeAlias = pydantic.fields.ModelField
 else:
     FieldInfo_: TypeAlias = pydantic.fields.FieldInfo
 ModelFields: TypeAlias = Dict[str, FieldInfo_]
-
-
-def get_modelfield_annotation(model: Type[pydantic.BaseModel], field_name: str):
-    # "annotation" exists in pydantic 1.10, but not 1.8 or 1.9
-    field = get_model_fields(model)[field_name]
-    return get_field_annotation(field)
-
-
-def get_field_annotation(field: FieldInfo_):
-    # "annotation" exists in pydantic 1.10, but not 1.8 or 1.9
-    if PYDANTIC_1:
-        return field.outer_type_
-    else:
-        return field.annotation
-
-
-def get_model_fields(model: Type[pydantic.BaseModel]) -> ModelFields:
-    if PYDANTIC_1:
-        return model.__fields__
-    else:
-        return model.model_fields
 
 
 def parse_obj(model: Type[PydModelT], obj: Any) -> PydModelT:
