391b018c54cb6e67ab0f49e7da0afbd9e52b97ff
Randall Pittman <pittmara@oregonstate.edu>
2025-01-16 15:10:50 -0800

    Attempt making __init__.py imports public by using relative import

--- src/nested_config/__init__.py
+++ src/nested_config/__init__.py
@@ -5,17 +5,17 @@ nested_config.expand_config().
 
 try:
     # Don't require pydantic
-    from nested_config._pydantic import (
+    from ._pydantic import (
         BaseModel,
         validate_config,
     )
 except ImportError:
     pass
 
-from nested_config.expand import ConfigExpansionError, expand_config
-from nested_config.loaders import (
+from .expand import ConfigExpansionError, expand_config
+from .loaders import (
     ConfigLoaderError,
     NoLoaderError,
     config_dict_loaders,
 )
-from nested_config.version import __version__
+from .version import __version__
